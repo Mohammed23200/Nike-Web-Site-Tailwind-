dbbde444c07f398f08af4022e8d4233454c5ca68
Mohammed23200 <mgsa112233@gmail.com>
2025-08-23 23:19:17 +0300

    spacial offer page

--- react-vite-tailwind-starter/src/components/Button.jsx
+++ react-vite-tailwind-starter/src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = ({label,iconURL}) => {
+const Button = ({label,iconURL,backgroundColor,textColor,borderColor}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none border-[#FF8559] bg-[#FF8559] rounded-full text-white'>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+    ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } 
+    rounded-full `}>
         {label}
         {iconURL && <img src={iconURL} alt="arrow right icon" className='ml-2 rounded-full w-5 h-5 ' />}
     </button>
--- react-vite-tailwind-starter/src/sections/SpacialOffer.jsx
+++ react-vite-tailwind-starter/src/sections/SpacialOffer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import { offer } from '../assets/images'
+import Button from '../components/Button'
+import {arrowRight} from '../assets/icons'
 const SpacialOffer = () => {
   return (
-    <section>SpacialOffer</section>
+    <section className='max-container flex justify-wrap items-center max-xl:flex-col-reverse gap-10 '> 
+      <div className='flex-1'>
+        <img src={offer} alt="" width={773} height={687} className='object-contain w-full' />
+      </div>
+       <div className='flex flex-1 flex-col '>
+        <h2 className="mt-10 font-palanquin capitalize text-4xl max-sm:text-[72px] max-sm:leading-[84px] font-bold lg:max-w-lg ">          
+          <span className="text-[#FF6452] ">Special</span> Offer
+        </h2>
+
+        <p className="mt-4 lg:max-w-lg font-montserrat text-slate-gray text-lg leading-7">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.       </p>
+        <p className='mt-6 lg:max-w-lg font-montserrat text-slate-gray text-lg leading-7'>
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label="Shop Now" iconURL={arrowRight}/>
+          <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor=" text-slate-gray"/>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
